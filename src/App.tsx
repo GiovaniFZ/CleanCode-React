d99c609b695c3dda603c6a660bdaaed51c22fde2
GiovaniFZ <giovanifinazzi@gmail.com>
2024-09-03 10:44:44 -0300

    refactor: separar Footer e Header em componentes separados, pois são usados poucas vezes no código

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { useState } from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App() {
   const [todos, setTodo] = useState<string[]>([])
 
-  const currentYear = new Date().getFullYear()
-
   return (
     <div>
-
-      <header>
-        <h1>My new innovate to-do list !</h1>
-          <button onClick={() => {}}>Add new todo</button>
-      </header>
+      <Header />
 
       <main>
         <h2>Advantages</h2>
@@ -32,11 +28,7 @@ export default function App() {
           {todos.map(todo => <li key={todo}>{todo}</li>)}
         </ul>
       </main>
-
-      <footer>
-
-        Copyright &copy; Rocketseat {currentYear}
-      </footer>
+    <Footer />
     </div>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>My new innovate to-do list !</h1>
+            <button onClick={() => { }}>Add new todo</button>
+        </header>
+    )
+}
